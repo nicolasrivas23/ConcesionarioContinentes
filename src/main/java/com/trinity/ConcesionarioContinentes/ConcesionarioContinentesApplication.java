b91778c1b19f91140a30ac4c5fa1b8ed6bdce747
nicolasrivas23 <nicolasrivasmeneses23@gmail.com>
2023-03-21 09:10:21 -0500

    Inicio del desarrollo

---
+++ src/main/java/com/trinity/ConcesionarioContinentes/ConcesionarioContinentesApplication.java
@@ -0,0 +1,13 @@
+package com.trinity.ConcesionarioContinentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionarioContinentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionarioContinentesApplication.class, args);
+	}
+
+}
